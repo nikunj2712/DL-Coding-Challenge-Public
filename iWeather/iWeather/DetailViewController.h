28ed3771982b9fa4d190afdfb1cf1d284be49526
Nikunj <nikunj@nikunj-mac.local>
2016-07-09 20:27:24 -0400

    In DetailVC, one can choose between 3 or 10 day forecast.

--- iWeather/iWeather/DetailViewController.h
+++ iWeather/iWeather/DetailViewController.h
@@ -14,5 +14,14 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelCondition;
 @property (nonatomic,strong) ListCityDetailsModel *selectedCity;
 @property (weak, nonatomic) IBOutlet UITableView *tableViewDayFoarecast;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentDaySelection;
+- (IBAction)segmentDayValueChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constTableDayForecastHeight;
+@property (weak, nonatomic) IBOutlet UIView *contentViewMain;
+@property (weak, nonatomic) IBOutlet UIImageView *imgCloud;
+@property (weak, nonatomic) IBOutlet UIImageView *imgWind;
+@property (weak, nonatomic) IBOutlet UIImageView *imgFan;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constCloudXPos;
+@property (weak, nonatomic) IBOutlet UIView *viewParentCloud;
 
 @end
--- iWeather/iWeather/DetailViewController.m
+++ iWeather/iWeather/DetailViewController.m
@@ -10,6 +10,7 @@
 
 @interface DetailViewController() <UITableViewDelegate,UITableViewDataSource> {
     NSMutableArray *arrayForecastDetails;
+    NSMutableArray *arrayForecast3Details;
 }
 
 @end
@@ -41,6 +42,22 @@
         [dictDetails setValue:[NSString stringWithFormat:@"%@°",[NSNumber numberWithInt:randNumMax]] forKey:@"tempmax"];
         [arrayForecastDetails addObject:dictDetails];
     }
+    
+    arrayForecast3Details = [NSMutableArray new];
+    for (int i=1; i<=3; i++) {
+        int randNumMin = rand() % ([maxForMin intValue] - [minForMin intValue]) + [minForMin intValue];
+        
+        int randNumMax = rand() % ([maxForMax intValue] - [minForMax intValue]) + [minForMax intValue];
+        
+        NSMutableDictionary *dictDetails = [NSMutableDictionary new];
+        NSString *strTime = [NSString stringWithFormat:@"Day %@",[NSNumber numberWithInt:i]];
+        [dictDetails setValue:strTime forKey:@"day"];
+        [dictDetails setValue:[NSString stringWithFormat:@"clear.gif"] forKey:@"iconurl"];
+        [dictDetails setValue:[NSString stringWithFormat:@"%@°",[NSNumber numberWithInt:randNumMin]] forKey:@"tempmin"];
+        [dictDetails setValue:[NSString stringWithFormat:@"%@°",[NSNumber numberWithInt:randNumMax]] forKey:@"tempmax"];
+        [arrayForecast3Details addObject:dictDetails];
+    }
+    self.tableViewDayFoarecast.scrollEnabled=FALSE;
 }
 
 -(void)viewWillAppear:(BOOL)animated{
@@ -49,14 +66,67 @@
     self.labelCondition.text = self.selectedCity.strWeather;
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+    [self.segmentDaySelection setSelectedSegmentIndex:0];
+    [self segmentDayValueChanged:self.segmentDaySelection];
+    [self animateWindMill];
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (self.segmentDaySelection.selectedSegmentIndex) {
+        //10
+            return arrayForecastDetails.count;
+    }
+    else{
+        //3day
+            return arrayForecast3Details.count;
+    }
     return arrayForecastDetails.count;
+}
+
+-(void)animateWindMill{
+    CABasicAnimation* animation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    animation.fromValue = [NSNumber numberWithFloat:0.0f];
+    animation.toValue = [NSNumber numberWithFloat: 2*M_PI];
+    animation.duration = 5.0f;
+    animation.repeatCount = INFINITY;
+    [self.imgFan.layer addAnimation:animation forKey:@"SpinAnimation"];
+    
+    [self.imgCloud setNeedsUpdateConstraints];
+    [self.viewParentCloud setNeedsUpdateConstraints];
+
+//    [UIView animateWithDuration:2.0f animations:^{
+//
+//           } completion:^(BOOL finished) {
+//
+//    }];
+    
+    [UIView animateWithDuration:15.0 delay:0 options: UIViewAnimationOptionRepeat | UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.constCloudXPos.constant = 200;
+        self.imgCloud.alpha = 0.2;
+        self.imgWind.alpha = 0.2;
+        [self.imgCloud layoutIfNeeded];
+        [self.viewParentCloud layoutIfNeeded];
+
+    } completion:^(BOOL finished) {
+        self.constCloudXPos.constant = -70;
+        self.imgCloud.alpha = 1;
+        self.imgWind.alpha = 1;
+    }];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-   
     NSDictionary *dictDetail = arrayForecastDetails[indexPath.row];
+    
+    if (self.segmentDaySelection.selectedSegmentIndex) {
+        //10
+        dictDetail = arrayForecastDetails[indexPath.row];    }
+    else{
+        //3day
+        dictDetail = arrayForecast3Details[indexPath.row];
+    }
+    
     NSString *strImg = [dictDetail valueForKey:@"iconurl"];
     NSString *strday = [dictDetail valueForKey:@"day"];
     NSString *strtempmin = [dictDetail valueForKey:@"tempmin"];
@@ -74,4 +144,34 @@
 
 }
 
+- (IBAction)segmentDayValueChanged:(id)sender {
+    
+    [self.tableViewDayFoarecast reloadData];
+    
+    UISegmentedControl *seg = (UISegmentedControl*)sender;
+    if (seg.selectedSegmentIndex) {
+        //10 day
+        
+        [self.tableViewDayFoarecast setNeedsUpdateConstraints];
+        [self.contentViewMain setNeedsUpdateConstraints];
+        [UIView animateWithDuration:0.5 animations:^{
+            self.constTableDayForecastHeight.constant = (arrayForecastDetails.count*55);
+            [self.tableViewDayFoarecast layoutIfNeeded];
+            [self.contentViewMain layoutIfNeeded];
+            [self.view layoutIfNeeded];
+        }];
+    }
+    else{
+        //3day
+        [self.tableViewDayFoarecast setNeedsUpdateConstraints];
+        [self.contentViewMain setNeedsUpdateConstraints];
+        [UIView animateWithDuration:0.5 animations:^{
+            self.constTableDayForecastHeight.constant = self.constTableDayForecastHeight.constant = (arrayForecast3Details.count*55);
+            [self.tableViewDayFoarecast layoutIfNeeded];
+            [self.contentViewMain layoutIfNeeded];
+            [self.view layoutIfNeeded];
+        }];
+    }
+    
+}
 @end
